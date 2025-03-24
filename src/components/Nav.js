05e556f5c9f60930949663f7f1fbf69b61c61cb3
Paul Kang <kbu715@gmail.com>
2025-03-24 18:12:48 +0900

    Nav 메뉴 active 상태 스타일 수정

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,28 @@
 export function Nav() {
+  const pathname = window.location.pathname;
+
   return /* HTML */ `
     <nav class="bg-white shadow-md p-2 sticky top-14">
       <ul class="flex justify-around">
-        <li><a href="/" class="text-gray-600">홈</a></li>
-        <li><a href="/profile" class="text-blue-600">프로필</a></li>
-        <li><a href="#" class="text-gray-600">로그아웃</a></li>
+        <li>
+          <a
+            href="/"
+            class="${pathname === "/" ? "text-blue-600" : "text-gray-600"}"
+            >홈</a
+          >
+        </li>
+        <li>
+          <a
+            href="/profile"
+            class="${pathname === "/profile"
+              ? "text-blue-600"
+              : "text-gray-600"}"
+            >프로필</a
+          >
+        </li>
+        <li>
+          <a href="#" class="text-gray-600">로그아웃</a>
+        </li>
       </ul>
     </nav>
   `;
