0727a09655e01d22fcca99a406284295c315f256
Paul Kang <kbu715@gmail.com>
2025-03-24 18:14:25 +0900

    history.pushState로 새로고침 없는 router 구현

--- src/main.js
+++ src/main.js
@@ -4,15 +4,40 @@ import { LoginPage } from "./pages/LoginPage";
 import { ErrorPage } from "./pages/ErrorPage";
 import { $ } from "./dom";
 
+const routes = {
+  "/": MainPage,
+  "/profile": ProfilePage,
+  "/login": LoginPage,
+  notFound: ErrorPage,
+};
+
+function navigate(pathname) {
+  window.history.pushState({}, "", pathname);
+  render();
+}
+
+window.addEventListener("popstate", render);
+
+document.addEventListener("click", (e) => {
+  const target = e.target.closest("a");
+
+  if (
+    target instanceof HTMLAnchorElement &&
+    target.href &&
+    target.origin === window.location.origin
+  ) {
+    e.preventDefault();
+    navigate(new URL(target.href).pathname);
+  }
+});
+
 function render() {
   const $root = $("#root");
 
-  $root.innerHTML = `
-    ${MainPage()}
-    ${ProfilePage()}
-    ${LoginPage()}
-    ${ErrorPage()}
-  `;
+  const pathname = window.location.pathname;
+  const comp = routes[pathname] || routes.notFound;
+
+  $root.innerHTML = comp();
 }
 
 render();
