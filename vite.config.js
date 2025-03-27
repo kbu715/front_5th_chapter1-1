40c5084972c6501597d58b88594c8c4aed083a99
Paul Kang <kbu715@gmail.com>
2025-03-27 15:20:26 +0900

    멀티 엔트리 빌드를 위한 설정

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vitest/config";
 import { loadEnv } from "vite";
+import { resolve } from "node:path";
 
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
@@ -12,5 +13,13 @@ export default ({ mode }) => {
       exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
     },
     base: env.VITE_BASE_URL,
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "index.html"),
+          hash: resolve(__dirname, "index.hash.html"),
+        },
+      },
+    },
   });
 };
